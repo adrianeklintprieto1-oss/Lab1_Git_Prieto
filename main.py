7bb1a90d10685e2f8ec186345804228d22d394f4
adrianeklintprieto1 <adrianeklintprieto1@gmail.com>
2026-02-04 10:26:25 +0800

    added gitignore

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-print("Lab 1: Version Control")
-print("Author: [adrianeklintprieto1]")
-print("Student ID: TUPM-25-4176")
-print( "Class Section: [BSECE-1A]")
-print("Login Feature Enabled")
+sdlkflskfsd
